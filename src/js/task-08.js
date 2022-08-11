b310a632a79d2b96a997271ac06a032b9ac8475a
pavel22shev <pavel22shev@gmail.com>
2022-08-11 18:58:34 +0300

    done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,14 @@
+const inputEl = document.querySelector(".logi m")
 
+inputEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "")
+        return alert("Please fill in all fields");
+
+    const result = { email: email.value, password: password.value };
+    console.log(result);
+    event.currentTarget.reset();
+})
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const btnEl = document.querySelector(".change-color")
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+
+const changeColorEl = document.querySelector(".change-color");
+
+const colorEl = document.querySelector(".color");
+
+changeColorEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = getRandomHexColor();
+});
